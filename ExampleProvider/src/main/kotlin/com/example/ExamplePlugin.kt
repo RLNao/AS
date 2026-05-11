033401abdac4644fc3a6f2dc3be68a963e1fb83b
RLNao <ryo.leeayen@gmail.com>
2026-05-11 23:44:29 +0200

    Build 7a2de011a8deebef6b48b883bbba6f65ddd82c1c

---
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -0,0 +1,26 @@
+package com.example
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class ExamplePlugin: Plugin() {
+    private var activity: AppCompatActivity? = null
+
+    override fun load(context: Context) {
+        activity = context as? AppCompatActivity
+
+        // C'est ici qu'on enregistre ton provider Anime-Sama pour qu'il apparaisse dans l'app
+        registerMainAPI(AnimeSama())
+
+        // On garde cette partie pour la gestion des paramètres (BlankFragment)
+        openSettings = {
+            val frag = BlankFragment(this)
+            activity?.let {
+                frag.show(it.supportFragmentManager, "Frag")
+            }
+        }
+    }
+}
